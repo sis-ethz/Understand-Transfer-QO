680edecfc063597ee933f81fe09ebf5583562787
yunjiazhang <yunjia@cs.wisc.edu>
2020-10-29 01:43:08 -0500

    update scripts

---
+++ interpretable_models/scripts/mlp/run_all_mlp.sh
@@ -0,0 +1,8 @@
+# rm mlp_all_features.log
+# nohup python3 mlp_all_features.py >> mlp_all_features.log &
+
+# rm mlp_key_features.log
+# nohup python3 mlp_key_features.py >> mlp_key_features.log &
+
+rm mlp_aug_features.log
+nohup python3 mlp_aug_features.py >> mlp_aug_features.log &
